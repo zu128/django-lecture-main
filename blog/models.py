06b3222a1808013f3bc5025150094910b203f506
zu128 <90748869+zu128@users.noreply.github.com>
2022-04-14 14:53:39 +0900

    django project

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from distutils.command.upload import upload
+from tokenize import blank_re
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     context ={'post': post,}
+#     return render(request, 'blog/single_post_page.html', context=context)
